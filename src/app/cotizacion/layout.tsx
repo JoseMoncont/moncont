bb78288c67abc88cfa700952fd8ee1a39552fb2d
Jose Montes <jmontesc@comfenalco.com>
2024-11-27 15:47:15 -0500

    layout and page

---
+++ src/app/cotizacion/layout.tsx
@@ -0,0 +1,19 @@
+import NavBar from "@/components/navbar";
+
+export default function CotizacionLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="es">
+      <body>
+        <div className="grid grid-rows-[0px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] dark:bg-slate-950">
+          {" "}
+          <NavBar></NavBar>
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/cotizacion/page.tsx
@@ -0,0 +1,9 @@
+export default function Cotizacion() {
+  return (
+    <>
+      <div>
+        <p>hello</p>
+      </div>
+    </>
+  );
+}
